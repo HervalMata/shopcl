b4acadf6ba0814a207e5e9c3cc406fe9d9c400f3
Herval Mata <hervalmataamparo@gmail.com>
2021-05-23 21:15:27 -0300

    feat:art: 1 - init project
    created project in modules

---
+++ ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/ShopclbackendApplication.java
@@ -0,0 +1,13 @@
+package com.shopcl.shopclbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopclbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopclbackendApplication.class, args);
+	}
+
+}
---
+++ ShopCLWebParent/ShopCLFrontend/src/main/java/com/shopcl/shopclfrontend/ShopclfrontendApplication.java
@@ -0,0 +1,13 @@
+package com.shopcl.shopclfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopclfrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopclfrontendApplication.class, args);
+	}
+
+}
