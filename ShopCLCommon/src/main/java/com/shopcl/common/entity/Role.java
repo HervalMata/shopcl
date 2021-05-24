241f11061d963a5be7402a48d09856c806d79482
Herval Mata <hervalmataamparo@gmail.com>
2021-05-24 19:03:12 -0300

    feat:art: 11 - add role and user
    define role and user entity

---
+++ ShopCLCommon/src/main/java/com/shopcl/common/entity/Role.java
@@ -0,0 +1,54 @@
+package com.shopcl.common.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "roles")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 40, nullable = false, unique = true)
+    private String name;
+
+    @Column(length = 150, nullable = false)
+    private String description;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        if (id == null) {
+            if (role.id != null) return false;
+        } else if (!id.equals(role.id)) return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
