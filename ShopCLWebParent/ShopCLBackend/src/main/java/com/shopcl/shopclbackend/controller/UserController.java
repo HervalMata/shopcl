6637ec0aefedf468e5b21dcfca4fbdf035f83ebb
Herval Mata <hervalmataamparo@gmail.com>
2021-05-24 23:01:37 -0300

    feat:art: 22 - add user controller
    define usercontroller and add listall method

---
+++ ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.shopcl.shopclbackend.controller;
+
+import com.shopcl.common.entity.User;
+import com.shopcl.shopclbackend.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String listAll(Model model) {
+        List<User> listUsers = userService.listAll();
+        model.addAttribute("listUsers", listUsers);
+        return "users";
+    }
+}
