bc4fb50a5447163ad1237caeb6f1d5ed64896723
Herval Mata <hervalmataamparo@gmail.com>
2021-05-27 14:32:56 -0300

    feat:art: 27 - add function for user form
    add function for the user form in user service and user controller

--- ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/controller/UserController.java
+++ ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.shopcl.shopclbackend.controller;
 
+import com.shopcl.common.entity.Role;
 import com.shopcl.common.entity.User;
 import com.shopcl.shopclbackend.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -22,5 +25,24 @@ public class UserController {
         List<User> listUsers = userService.listAll();
         model.addAttribute("listUsers", listUsers);
         return "users";
+    }
+
+    @GetMapping("/users/new")
+    public String newUser(Model model) {
+        List<Role> listRoles = userService.listRoles();
+        User user = new User();
+        user.setEnabled(true);
+        model.addAttribute("user", user);
+        model.addAttribute("listRoles", listRoles);
+        model.addAttribute("pageTitle", "Cadastrar Novo Usuário");
+        return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user, RedirectAttributes redirectAttributes) {
+        System.out.println(user);
+        userService.save(user);
+        redirectAttributes.addFlashAttribute("message", "O usuário foi salvo com sucesso.");
+        return "redirect:/users";
     }
 }
--- ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/service/UserService.java
+++ ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/service/UserService.java
@@ -1,7 +1,11 @@
 package com.shopcl.shopclbackend.service;
 
+import com.shopcl.common.entity.Role;
 import com.shopcl.common.entity.User;
+import com.shopcl.shopclbackend.repository.RoleRepository;
 import com.shopcl.shopclbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -13,14 +17,33 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    @Autowired
+    public UserService(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public List<User> listAll() {
         List<User> userList = new ArrayList<>();
         userRepository.findAll().forEach(userList::add);
         return userList;
+    }
+
+    public List<Role> listRoles() {
+        return (List<Role>) roleRepository.findAll();
+    }
+
+    public void save(User user) {
+        encodePassword(user);
+        userRepository.save(user);
+    }
+
+    private void encodePassword(User user) {
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
     }
 }
