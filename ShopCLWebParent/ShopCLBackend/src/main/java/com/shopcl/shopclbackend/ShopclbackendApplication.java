8caf88f8956bf28430a15823f105a46053c868a5
Herval Mata <hervalmataamparo@gmail.com>
2021-05-27 16:17:12 -0300

    feat:art: 31 - defined email unique function in user service

--- ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/ShopclbackendApplication.java
+++ ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/ShopclbackendApplication.java
@@ -3,11 +3,9 @@ package com.shopcl.shopclbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EntityScan({"com.shopcl.common.entity"})
-@ComponentScan(basePackages= "com.shopcl.shopclbackend.security")
+@EntityScan({"com.shopcl.common.entity", "com.shopcl.shopclbackend.*"})
 public class ShopclbackendApplication {
 
 	public static void main(String[] args) {
