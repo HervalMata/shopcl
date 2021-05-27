f0d62c4805247c2868eb45c81c8119b69a38f6eb
Herval Mata <hervalmataamparo@gmail.com>
2021-05-27 16:11:58 -0300

    feat:art: 30 - tried to fix passwordencoder issue

--- ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/ShopclbackendApplication.java
+++ ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/ShopclbackendApplication.java
@@ -3,9 +3,11 @@ package com.shopcl.shopclbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EntityScan({"com.shopcl.common.entity", "com.shopcl.shopclbackend.user"})
+@EntityScan({"com.shopcl.common.entity"})
+@ComponentScan(basePackages= "com.shopcl.shopclbackend.security")
 public class ShopclbackendApplication {
 
 	public static void main(String[] args) {
--- ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/security/WebSecurityConfig.java
+++ ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/security/WebSecurityConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
-    public PasswordEncoder PasswordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
