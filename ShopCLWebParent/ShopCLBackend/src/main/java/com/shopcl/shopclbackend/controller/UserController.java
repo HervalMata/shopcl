fdd509df64056700ba7ee30d3e9891a4b24e03a7
Herval Mata <hervalmataamparo@gmail.com>
2021-05-27 22:39:49 -0300

    feat:art: 40 - define edit user in user controller

--- ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/controller/UserController.java
+++ ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/controller/UserController.java
@@ -2,10 +2,12 @@ package com.shopcl.shopclbackend.controller;
 
 import com.shopcl.common.entity.Role;
 import com.shopcl.common.entity.User;
+import com.shopcl.shopclbackend.error.UserNotFoundException;
 import com.shopcl.shopclbackend.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -44,5 +46,20 @@ public class UserController {
         userService.save(user);
         redirectAttributes.addFlashAttribute("message", "O usuário foi salvo com sucesso.");
         return "redirect:/users";
+    }
+
+    @GetMapping("/users/edit/{id}")
+    public String editUser(@PathVariable(name = "id") Long id, Model model, RedirectAttributes redirectAttributes) {
+        try {
+            User user = userService.get(id);
+            List<Role> listRoles = userService.listRoles();
+            model.addAttribute("user", user);
+            model.addAttribute("listRoles", listRoles);
+            model.addAttribute("pageTitle", "Editar Usuário (ID: " + id + ")");
+            return "user_form";
+        } catch (UserNotFoundException ex) {
+            redirectAttributes.addFlashAttribute("messageError", ex.getMessage());
+            return "redirect:/users";
+        }
     }
 }
