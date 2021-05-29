8e659cc9faee7f170ad31a271fde72786c80a1ec
Herval Mata <hervalmataamparo@gmail.com>
2021-05-29 14:32:20 -0300

    feat:art: 57 - changed crudrepository to pagingandsortingrepository

--- ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/repository/UserRepository.java
+++ ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/repository/UserRepository.java
@@ -3,12 +3,12 @@ package com.shopcl.shopclbackend.repository;
 import com.shopcl.common.entity.User;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
     @Query("SELECT u FROM User u WHERE u.email = :email")
     public User getUserByEmail(@Param("email") String email);
