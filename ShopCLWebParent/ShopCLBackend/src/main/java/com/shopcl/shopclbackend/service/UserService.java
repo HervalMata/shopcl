ce52d86e2588990ba008f8773ae1287feb9bcd6d
Herval Mata <hervalmataamparo@gmail.com>
2021-05-30 20:44:48 -0300

    feat:art: 67 - add search keyword in list by page method of user service

--- ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/service/UserService.java
+++ ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/service/UserService.java
@@ -96,10 +96,13 @@ public class UserService {
         userRepository.updateEnabledStatus(id, enabled);
     }
 
-    public Page<User> listByPage(int pageNum, String sortField, String sortDir) {
+    public Page<User> listByPage(int pageNum, String sortField, String sortDir, String keyword) {
         Sort sort = Sort.by(sortField);
         sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
         Pageable pageable = PageRequest.of(pageNum - 1, USERS_PER_PAGE, sort);
+        if (keyword != null) {
+            return userRepository.findAll(keyword, pageable);
+        }
         return userRepository.findAll(pageable);
     }
 
