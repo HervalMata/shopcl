5fbb054a0fe6ded83021984fea35368dc3638044
Herval Mata <hervalmataamparo@gmail.com>
2021-06-03 15:35:48 -0300

    feat:art: 83 - redefine filename and encoding in abstract exporter

--- ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/util/AbstractExporter.java
+++ ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/util/AbstractExporter.java
@@ -1,6 +1,8 @@
 package com.shopcl.shopclbackend.util;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -8,13 +10,12 @@ import java.util.Date;
 public class AbstractExporter {
 
     public void setResponseHeader(HttpServletResponse response, String contentType,
-                                  String extension) {
+                                  String extension) throws UnsupportedEncodingException {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
         String timestamp = dateFormat.format(new Date());
-        String fileName = "users_" + timestamp + extension;
-        String utf = "UTF-8";
+        String fileName = URLEncoder.encode("users_" + timestamp + extension, "utf-8");
         response.setContentType(contentType);
-        response.setCharacterEncoding(utf);
+        response.setCharacterEncoding("UTF-8");
         String headerKey = "Content-Disposition";
         String headerValue = "attachment; filename=" + fileName;
         response.setHeader(headerKey, headerValue);
