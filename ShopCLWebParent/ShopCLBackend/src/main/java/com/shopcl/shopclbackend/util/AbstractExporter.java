ffade59c0a039b81f9c8d62bfae944a206dc49bb
Herval Mata <hervalmataamparo@gmail.com>
2021-06-03 14:10:16 -0300

    feat:art: 81 - add utf-8 feature in abstract exporter class

--- ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/util/AbstractExporter.java
+++ ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/util/AbstractExporter.java
@@ -12,7 +12,9 @@ public class AbstractExporter {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
         String timestamp = dateFormat.format(new Date());
         String fileName = "users_" + timestamp + extension;
+        String utf = "UTF-8";
         response.setContentType(contentType);
+        response.setCharacterEncoding(utf);
         String headerKey = "Content-Disposition";
         String headerValue = "attachment; filename=" + fileName;
         response.setHeader(headerKey, headerValue);
