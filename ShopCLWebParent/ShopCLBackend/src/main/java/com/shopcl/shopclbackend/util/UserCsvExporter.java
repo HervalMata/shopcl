999f4690feb69ae6d75a4bf94c1dcbee2876e221
Herval Mata <hervalmataamparo@gmail.com>
2021-06-03 15:39:12 -0300

    feat:art: 84 - add utf-8 encoding in user csv exporter

--- ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/util/UserCsvExporter.java
+++ ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/util/UserCsvExporter.java
@@ -7,13 +7,17 @@ import org.supercsv.prefs.CsvPreference;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.List;
 
 public class UserCsvExporter extends AbstractExporter {
 
     public void export(List<User> listUsers, HttpServletResponse response) throws IOException {
         super.setResponseHeader(response, "text/csv", ".csv");
-        ICsvBeanWriter csvBeanWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+        Writer writer = new OutputStreamWriter(response.getOutputStream(), "utf-8");
+        writer.write('\uFEFF');
+        ICsvBeanWriter csvBeanWriter = new CsvBeanWriter(writer, CsvPreference.STANDARD_PREFERENCE);
         String[] csvHeader = {"Usuário ID", "Email", "Nome", "Sobrenome", "Funções", "Habilitado"};
         String[] fieldMapping = {"id", "email", "firstName", "lastName", "roles", "enabled"};
         csvBeanWriter.writeHeader(csvHeader);
