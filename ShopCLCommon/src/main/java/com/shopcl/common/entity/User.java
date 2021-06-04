a38b4babe39a2df276420691ae40f422b932888f
Herval Mata <hervalmataamparo@gmail.com>
2021-06-03 22:33:48 -0300

    feat:art: 98 - add fetchtype.eager in user

--- ShopCLCommon/src/main/java/com/shopcl/common/entity/User.java
+++ ShopCLCommon/src/main/java/com/shopcl/common/entity/User.java
@@ -36,7 +36,7 @@ public class User {
 
     private boolean enabled;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
