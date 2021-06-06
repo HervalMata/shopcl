ff64593b216b2f2f4e18489b5679f3cc62a63ef0
Herval Mata <hervalmataamparo@gmail.com>
2021-06-06 14:55:37 -0300

    feat:art: 117 - redefine firstpartofemail in getdirecturltoaffecteduser of directutil

--- ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/util/DirectUtil.java
+++ ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/util/DirectUtil.java
@@ -5,7 +5,7 @@ import com.shopcl.common.entity.User;
 public class DirectUtil {
 
     public static String getRedirectURLtoAffectedUser(User user) {
-        String firstPartOfEmail = user.getEmail().split("@")[0];
+        String firstPartOfEmail = user.getEmail();
         return "redirect:/users/page/1?sortField=id&sortDir=asc&keyword=" + firstPartOfEmail;
     }
 }
