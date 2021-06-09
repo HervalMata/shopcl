c2122ca0779297c001bbb8c8f85058ba6e0641a7
Herval Mata <hervalmataamparo@gmail.com>
2021-06-08 23:50:26 -0300

    feat:art: 126 - created category model and repository

---
+++ ShopCLCommon/src/main/java/com/shopcl/common/entity/Category.java
@@ -0,0 +1,35 @@
+package com.shopcl.common.entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "categories")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(length = 128, nullable = false, unique = true)
+    private String name;
+
+    @Column(length = 64, nullable = false, unique = true)
+    private String alias;
+
+    @Column(length = 128, nullable = false)
+    private String image;
+
+    private boolean enabled;
+}
