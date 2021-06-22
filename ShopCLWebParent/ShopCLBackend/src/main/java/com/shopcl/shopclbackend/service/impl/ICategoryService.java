5af53a76af0a3df633a37ea7cc49c0eadc8a3934
Herval Mata <hervalmataamparo@gmail.com>
2021-06-22 20:22:11 -0300

    feat:art: 132 - added listcategoriesusedinform in icategoryservice

--- ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/service/impl/ICategoryService.java
+++ ShopCLWebParent/ShopCLBackend/src/main/java/com/shopcl/shopclbackend/service/impl/ICategoryService.java
@@ -2,9 +2,10 @@ package com.shopcl.shopclbackend.service.impl;
 
 import com.shopcl.common.entity.Category;
 
-import java.util.List;
+import java.util.List;0
 
 public interface ICategoryService {
 
     public List<Category> listAll();
+    public List<Category> listCategoriesUsedInForm();
 }
